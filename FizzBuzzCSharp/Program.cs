286cadee6bf766e3dad73234acd639b0b016807f
jackcarey <35701383+jackcarey@users.noreply.github.com>
2023-12-13 15:40:32 +0000

    added basic FizzBuzz using function call

--- FizzBuzzCSharp/Program.cs
+++ FizzBuzzCSharp/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int start = 1;
+            int end = 100;
+            Dictionary<int, string> multiples = new()
+            {
+                [3] = "Fizz",
+                [5] = "Buzz"
+            };
+            if (args.Length > 0)
+            {
+                try
+                {
+                    int startArg = int.Parse(args[0]);
+                    if (startArg <= 0)
+                    {
+                        throw new FormatException("'start' must be greater than 0.");
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Using 1 for start. Error: " + e.Message);
+                }
+            }
+            for (int i = start; i < end; ++i)
+            {
+                string result = MultipleString(ref i, ref multiples);
+                Console.WriteLine(result);
+            }
+        }
+
+        static string MultipleString(ref int val, ref Dictionary<int, string> multiples)
+        {
+            string resultStr = "";
+            foreach (var multiple in multiples)
+            {
+                if (val % multiple.Key == 0)
+                {
+                    resultStr += multiple.Value;
+                }
+            }
+            if (resultStr.Length == 0)
+            {
+                resultStr = val.ToString();
+            }
+            return resultStr;
         }
     }
 }
