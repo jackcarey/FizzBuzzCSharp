9b10ccd5b560d760e7128726c2aa296d588a8d55
jackcarey <35701383+jackcarey@users.noreply.github.com>
2023-12-13 17:03:48 +0000

    added async version
    using Tasks to create a thread pool, with a List to track them

--- FizzBuzzCSharp/Program.cs
+++ FizzBuzzCSharp/Program.cs
@@ -1,18 +1,18 @@
 ﻿namespace FizzBuzzCSharp
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Running default FizzBuzz...");
-            FizzBuzz.Run();
+            Console.WriteLine("Running async FizzBuzz...");
+            FizzBuzz.RunAsync();
         }
     }
     public class FizzBuzz
     {
         public static void Run(int start = 1, int end = 100, Dictionary<int, string> multiples = null)
         {
-            //FizzBuzz will always run in ascending order, so swap the argments if they are in the wrong positions
+            //FizzBuzz will always run in ascending order, so swap the arguments if they are in the wrong positions
             if (end < start)
             {
                 int temp = end;
@@ -37,6 +37,38 @@
             }
         }
 
+        public static async Task RunAsync(int start = 1, int end = 100, Dictionary<int, string> multiples = null)
+        {
+            //FizzBuzz will always run in ascending order, so swap the arguments if they are in the wrong positions
+            if (end < start)
+            {
+                int temp = end;
+                end = start;
+                start = temp;
+            }
+
+            //If no custom arguments are provided, sensible defaults for the multiples dictionary are 'Fizz' and 'Buzz'
+            if (multiples == null || multiples.Count == 0)
+            {
+                multiples = new Dictionary<int, string>
+                {
+                    { 3, "Fizz" },
+                    { 5, "Buzz" }
+                };
+            }
+
+            List<Task<string>> taskPool = new List<Task<string>>();
+            for (int i = start; i <= end; ++i)
+            {
+                taskPool.Add(MultipleStringTask(i, multiples));
+            }
+            await Task.WhenAll(taskPool);
+            foreach (var task in taskPool.ToArray())
+            {
+                Console.WriteLine($"{task.Result}");
+            }
+        }
+
         public static string MultipleString(int val, ref Dictionary<int, string> multiples)
         {
             string resultStr = "";
@@ -52,6 +84,26 @@
                 resultStr = val.ToString();
             }
             return resultStr;
+        }
+
+        public static Task<string> MultipleStringTask(int val, Dictionary<int, string> multiples)
+        {
+            return Task<string>.Factory.StartNew(() =>
+            {
+                string resultStr = "";
+                foreach (var multiple in multiples)
+                {
+                    if (val % multiple.Key == 0)
+                    {
+                        resultStr += multiple.Value;
+                    }
+                }
+                if (resultStr.Length == 0)
+                {
+                    resultStr = val.ToString();
+                }
+                return resultStr;
+            });
         }
     }
 }
