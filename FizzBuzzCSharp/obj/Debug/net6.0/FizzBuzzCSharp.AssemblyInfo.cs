2eebfb26f2c6ecb2ea28519baf14a69d59b76f4b
jackcarey <35701383+jackcarey@users.noreply.github.com>
2023-12-13 17:12:31 +0000

    added VS project files

--- FizzBuzzCSharp/obj/Debug/net6.0/FizzBuzzCSharp.AssemblyInfo.cs
+++ FizzBuzzCSharp/obj/Debug/net6.0/FizzBuzzCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FizzBuzzCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9031270c3cf2a1193af62775164ed2dd9e0050f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FizzBuzzCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FizzBuzzCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
